bf1a1038fded637a40a9ebdbd409e46e4ef5d164
S. Brent Faulkner <brentf@unwwwired.net>
2008-11-20 10:51:25 -0500

    initial commit

---
+++ README.markdown
@@ -0,0 +1,7 @@
+# Conductor Application
+
+This is a base rails application with the following features:
+
+  - [Authlogic](http://github.com/binarylogic/authlogic) based authentication, including password reset
+
+Copyright (c) 2008 [unwwwired.net](http://unwwwired.net), released under the MIT license
---
+++ test/functional/users_controller_test.rb
@@ -0,0 +1,42 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionController::TestCase
+=begin
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create user" do
+    assert_difference('User.count') do
+      post :create, :user => { }
+    end
+
+    assert_redirected_to user_path(assigns(:user))
+  end
+=end
+  test "should show user" do
+    get :show
+    assert_response :success
+  end
+
+=begin
+  test "should get edit" do
+    get :edit, :id => users(:one).id
+    assert_response :success
+  end
+
+  test "should update user" do
+    put :update, :id => users(:one).id, :user => { }
+    assert_redirected_to user_path(assigns(:user))
+  end
+
+  test "should destroy user" do
+    assert_difference('User.count', -1) do
+      delete :destroy, :id => users(:one).id
+    end
+
+    assert_redirected_to users_path
+  end
+=end
+end
