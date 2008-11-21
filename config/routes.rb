796b76130ba3341f19631f46d46c7d1a3e862f58
S. Brent Faulkner <brentf@unwwwired.net>
2008-11-20 23:05:34 -0500

    strict xhtml + signup/registration routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 ActionController::Routing::Routes.draw do |map|
-  map.login "/login", :controller => "user_sessions", :action => "new"
-  map.logout "/logout", :controller => "user_sessions", :action => "destroy"
+  map.login '/login', :controller => 'user_sessions', :action => 'new'
+  map.logout '/logout', :controller => 'user_sessions', :action => 'destroy'
 
   map.register '/register', :controller => 'users', :action => 'create'
   map.signup '/signup', :controller => 'users', :action => 'new'
 
-  map.resource :account, :controller => "users"
+  map.resource :account, :controller => 'users'
   map.resources :password_resets
   map.resources :users
   map.resource :user_session
 
-  map.default "/", :controller => "user_sessions", :action => "new"
+  map.default '/', :controller => 'user_sessions', :action => 'new'
 end
